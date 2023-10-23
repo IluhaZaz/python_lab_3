0d5d64f03e8663295b55e2a59731bf682c3f3214
Acer <Acer@DESKTOP-5LU3HEA>
2023-10-23 21:04:48 +0300

    Создание другого датасета, одино поле на комментарии, проблемма со 2й аннотацией

--- main_window.py
+++ main_window.py
@@ -7,7 +7,8 @@ import sys
 import os
 
 from task_5 import Iterator
-from task_1 import create_annotaion
+import task_1
+import task_2
 
 class ScrollLabel(QScrollArea):
  
@@ -33,16 +34,17 @@ class Window(QWidget):
     def initUI(self):
         self.choose_dir_button = QtWidgets.QPushButton(self)
         self.choose_dir_button.setText("Select dataset folder")
-        self.choose_dir_button.setFixedWidth(300)
+        self.choose_dir_button.adjustSize()
         self.choose_dir_button.clicked.connect(self.choose_dir)
 
         self.dir_label = QtWidgets.QLabel(self)
         self.dir_label.setText("")
+        self.dir_label.adjustSize()
 
         self.create_ann_btn = QtWidgets.QPushButton(self)
         self.create_ann_btn.setText("Create annotation for your dataset")
-        self.create_ann_btn.setFixedWidth(300)
-        self.create_ann_btn.clicked.connect(self.create_annotation_for_btn)
+        self.create_ann_btn.adjustSize()
+        self.create_ann_btn.clicked.connect(self.create_annotation)
 
         self.ann_success_label = QtWidgets.QLabel(self)
 
@@ -52,33 +54,32 @@ class Window(QWidget):
         self.good_button.clicked.connect(self.get_next_good)
 
         
-        self.good_review = ScrollLabel(self)
-        self.good_review.setFixedSize(1550, 300)
-
-        self.bad_review = ScrollLabel(self)
-        self.bad_review.setFixedSize(1550, 300)
+        self.review_label = ScrollLabel(self)
 
         self.bad_button = QtWidgets.QPushButton(self)
-        self.bad_button.move(940, 0)
         self.bad_button.setText("Show next bad review")
         self.bad_button.adjustSize()
         self.bad_button.clicked.connect(self.get_next_bad)
 
+        self.copy_dataset_btn = QtWidgets.QPushButton(self)
+        self.copy_dataset_btn.setText("Create dataset with different struct")
+        self.copy_dataset_btn.adjustSize()
+        self.copy_dataset_btn.clicked.connect(self.copy_dataset)
 
         grid = QGridLayout()
-        grid.setSpacing(20)
-        grid.addWidget(self.choose_dir_button, 0, 0)
-        grid.addWidget(self.dir_label, 0, 1)
-        grid.addWidget(self.create_ann_btn, 1, 0)    
-        grid.addWidget(self.ann_success_label, 1, 1)
-        grid.addWidget(self.good_button, 2, 0)
-        grid.addWidget(self.good_review, 2, 1)
-        grid.addWidget(self.bad_button, 3, 0)
-        grid.addWidget(self.bad_review, 3, 1)
+        grid.setSpacing(7)
+        grid.addWidget(self.choose_dir_button, 1, 0)
+        grid.addWidget(self.dir_label, 1, 1)
+        grid.addWidget(self.create_ann_btn, 2, 0)    
+        grid.addWidget(self.ann_success_label, 2, 1)
+        grid.addWidget(self.good_button, 3, 0)
+        grid.addWidget(self.bad_button, 4, 0)
+        grid.addWidget(self.review_label, 3, 3, 2, 1)
+        grid.addWidget(self.copy_dataset_btn, 5, 0)
         self.setLayout(grid)
         
         self.setWindowTitle("3rd lab")
-        self.setGeometry(0, 0, 1920, 1080)
+        self.setGeometry(0, 0, 1900, 950)
 
     def choose_dir(self):
         self.dataset_path = os.path.abspath(QtWidgets.QFileDialog.getExistingDirectory(self, 'Select dataset folder'))
@@ -87,9 +88,9 @@ class Window(QWidget):
         self.good_iterator = Iterator(self.dataset_path, "good")
         self.bad_iterator = Iterator(self.dataset_path, "bad")
         
-    def create_annotation_for_btn(self):
+    def create_annotation(self):
         try:
-            if create_annotaion("ann1.csv", self.dataset_path) == True:
+            if task_1.create_annotaion("ann1.csv", self.dataset_path) == True:
                 self.ann_success_label.setText("Success")
             else:
                 self.ann_success_label.setText("Error")
@@ -99,14 +100,22 @@ class Window(QWidget):
     def get_next_good(self):
         try:
             with open(next(self.good_iterator), "r", encoding="utf-8") as file:
-                self.good_review.setText(" ".join(file.readlines()))
+                self.review_label.setText(" ".join(file.readlines()))
         except AttributeError:
             self.error_window("You should choose directory first!", "Error")
 
     def get_next_bad(self):
         try:
             with open(next(self.bad_iterator), "r", encoding="utf-8") as file:
-                self.bad_review.setText(" ".join(file.readlines()))
+                self.review_label.setText(" ".join(file.readlines()))
+        except AttributeError:
+            self.error_window("You should choose directory first!", "Error")
+    
+    def copy_dataset(self):
+        try:
+            self.copy_dir = os.path.abspath(QtWidgets.QFileDialog.getExistingDirectory(self, 'Select dataset copy folder'))
+            task_2.copy_dataset(self.copy_dir, self.dataset_path)
+            task_2.create_annotaion(self.copy_dir, self.dataset_path, "ann2.csv")
         except AttributeError:
             self.error_window("You should choose directory first!", "Error")
 
