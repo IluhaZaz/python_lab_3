83ae4faf16a9d19b9864177e984cdcff456f6e8e
Acer <Acer@DESKTOP-5LU3HEA>
2023-10-24 17:58:54 +0300

    Кажется добавлен весь необходимый функционал

--- main_window.py
+++ main_window.py
@@ -1,4 +1,4 @@
-from PyQt5 import QtWidgets, Qt
+from PyQt5 import QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
@@ -9,10 +9,10 @@ import os
 from task_5 import Iterator
 import task_1
 import task_2
+import task_3
+
 
 class ScrollLabel(QScrollArea):
- 
-    # constructor
     def __init__(self, *args, **kwargs):
         QScrollArea.__init__(self, *args, **kwargs)
         self.setWidgetResizable(True)
@@ -26,11 +26,12 @@ class ScrollLabel(QScrollArea):
     def setText(self, text):
         self.label.setText(text)
 
+
 class Window(QWidget):
     def __init__(self):
         super(QWidget, self).__init__()
         self.initUI()
-        
+
     def initUI(self):
         self.choose_dir_button = QtWidgets.QPushButton(self)
         self.choose_dir_button.setText("Select dataset folder")
@@ -53,7 +54,6 @@ class Window(QWidget):
         self.good_button.adjustSize()
         self.good_button.clicked.connect(self.get_next_good)
 
-        
         self.review_label = ScrollLabel(self)
 
         self.bad_button = QtWidgets.QPushButton(self)
@@ -66,28 +66,36 @@ class Window(QWidget):
         self.copy_dataset_btn.adjustSize()
         self.copy_dataset_btn.clicked.connect(self.copy_dataset)
 
+        self.random_dataset_btn = QtWidgets.QPushButton(self)
+        self.random_dataset_btn.setText("Create dataset with random instances")
+        self.random_dataset_btn.adjustSize()
+        self.random_dataset_btn.clicked.connect(self.create_rand_dataset)
+
         grid = QGridLayout()
         grid.setSpacing(7)
         grid.addWidget(self.choose_dir_button, 1, 0)
         grid.addWidget(self.dir_label, 1, 1)
-        grid.addWidget(self.create_ann_btn, 2, 0)    
+        grid.addWidget(self.create_ann_btn, 2, 0)
         grid.addWidget(self.ann_success_label, 2, 1)
         grid.addWidget(self.good_button, 3, 0)
-        grid.addWidget(self.bad_button, 4, 0)
-        grid.addWidget(self.review_label, 3, 3, 2, 1)
-        grid.addWidget(self.copy_dataset_btn, 5, 0)
+        grid.addWidget(self.bad_button, 5, 0)
+        grid.addWidget(self.review_label, 3, 3, 3, 1)
+        grid.addWidget(self.copy_dataset_btn, 6, 0)
+        grid.addWidget(self.random_dataset_btn, 7, 0)
         self.setLayout(grid)
-        
+
         self.setWindowTitle("3rd lab")
         self.setGeometry(0, 0, 1900, 950)
 
     def choose_dir(self):
-        self.dataset_path = os.path.abspath(QtWidgets.QFileDialog.getExistingDirectory(self, 'Select dataset folder'))
+        self.dataset_path = os.path.abspath(
+            QtWidgets.QFileDialog.getExistingDirectory(self, "Select dataset folder")
+        )
         self.dir_label.setText(f"Chose directory: {self.dataset_path}")
         self.dir_label.adjustSize()
         self.good_iterator = Iterator(self.dataset_path, "good")
         self.bad_iterator = Iterator(self.dataset_path, "bad")
-        
+
     def create_annotation(self):
         try:
             if task_1.create_annotaion("ann1.csv", self.dataset_path) == True:
@@ -96,7 +104,7 @@ class Window(QWidget):
                 self.ann_success_label.setText("Error")
         except AttributeError:
             self.error_window("You should choose directory first!", "Error")
-      
+
     def get_next_good(self):
         try:
             with open(next(self.good_iterator), "r", encoding="utf-8") as file:
@@ -110,17 +118,21 @@ class Window(QWidget):
                 self.review_label.setText(" ".join(file.readlines()))
         except AttributeError:
             self.error_window("You should choose directory first!", "Error")
-    
+
     def copy_dataset(self):
         try:
-            self.copy_dir = os.path.abspath(QtWidgets.QFileDialog.getExistingDirectory(self, 'Select dataset copy folder'))
+            self.copy_dir = os.path.abspath(
+                QtWidgets.QFileDialog.getExistingDirectory(
+                    self, "Select dataset copy folder"
+                )
+            )
             task_2.copy_dataset(self.copy_dir, self.dataset_path)
             task_2.create_annotaion(self.copy_dir, self.dataset_path, "ann2.csv")
         except AttributeError:
             self.error_window("You should choose directory first!", "Error")
 
     def error_window(self, text, title):
-        dialog=QDialog()
+        dialog = QDialog()
 
         label = QLabel(dialog)
         label.setText(text)
@@ -128,7 +140,7 @@ class Window(QWidget):
 
         btn = QPushButton(dialog)
         btn.setText("Ok")
-        btn.move(50,50)
+        btn.move(50, 50)
         btn.clicked.connect(lambda: dialog.hide())
 
         lay = QVBoxLayout()
@@ -140,16 +152,26 @@ class Window(QWidget):
         dialog.setWindowTitle(title)
         dialog.exec_()
 
+    def create_rand_dataset(self):
+        try:
+            self.copy_rand_dir = os.path.abspath(
+                QtWidgets.QFileDialog.getExistingDirectory(
+                    self, "Select dataset random copy folder"
+                )
+            )
+            task_3.create_annotaion(self.copy_rand_dir, self.dataset_path, "ann3.csv")
+        except AttributeError:
+            self.error_window("You should choose directory first!", "Error")
+
 
 def application():
     app = QApplication(sys.argv)
 
     window = Window()
-    
+
     window.show()
 
     sys.exit(app.exec_())
-    
 
 
 if __name__ == "__main__":
